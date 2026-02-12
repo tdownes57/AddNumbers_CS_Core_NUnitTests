9ec03baed6c9be5d27a1a4c09e3b690ed32eb173
Thomas C. Downes <tomdownes1@gmail.com>
2026-02-11 22:15:13 -0800

    Revise console output for adding numbers algorithm
    Updated console messages to reflect the purpose of the application and provided a detailed explanation of the algorithm used for adding numbers as strings.

--- AddNumbers_NodeJS/app - Copy.js
+++ AddNumbers_NodeJS/app - Copy.js
@@ -1,8 +1,12 @@
 'use strict';
 
-console.log('Hello world');
+console.log('Adding Numbers of Any Length');
 
-//
+console.log("Adding Numbers of Any Length demonstrates a straightforword algorithm for adding numbers of any size, no matter how many digits, using only strings & characters (versus numeric types).");
+
+console.log("This includes code for a straightforward algorithm for adding (& subtracting) numbers of any size, no matter how many digits. You might have noticed that most calculating methods are limited in how many digits they process. Most calculators are limited to about 5 to 12 digits. Not this one. It can easily add numbers the length of an average paragraph (such as this one), or even longer. The reason is, it treats the numbers as strings, not as numeric data types. It receives two strings of digits as input, and keeps the input as strings rather than converted to numeric data types. Numeric data types are always limited in size, whereas strings can be comparatively unlimited in size. It demonstrates an understanding of core principles of how mathematics and programming intersect. Instead of relying on any ready-made mathematical libraries, it shows that the algorithms of addition and subtraction can be re-engineered from scratch."); 
+
+    //
 //  https://stackoverflow.com/questions/7942398/nested-objects-in-javascript-best-practices
 //
 var DigitAndCarry = {
